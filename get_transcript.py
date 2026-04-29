a8774eb8eb181ac32afb92fef844dc19a1b96e86
ramacharya06 <rama14062006@gmail.com>
2026-04-29 15:37:42 +0530

    feat: add cookie authentication support to YouTube transcript fetching

--- get_transcript.py
+++ get_transcript.py
@@ -5,13 +5,13 @@ from video_id_extractor import get_video_id
 
 
 def get_transcript(video_url):
-    api = YouTubeTranscriptApi()
-    transcript_data = api.fetch(get_video_id(video_url), languages=['en'])
-    transcript = " ".join([entry.text for entry in transcript_data])    
+    transcript_data = YouTubeTranscriptApi.get_transcript(
+        get_video_id(video_url), 
+        languages=['en'],
+        cookies='cookies.txt'
+    )
+    transcript = " ".join([entry['text'] for entry in transcript_data])    
     return transcript
-# Print transcript
-# for line in transcript:
-#     print(line['text'])
 
 if __name__ == "__main__":
    print(get_transcript("https://www.youtube.com/watch?v=gzt52Trk9w0"))
